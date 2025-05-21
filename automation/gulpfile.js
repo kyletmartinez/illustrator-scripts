d85bf833cba32270b09b1de6bd8064354c83937c
Kyle Martinez <kyletmartinez@gmail.com>
2025-05-21 11:36:08 -0500

    fix gulp

--- automation/gulpfile.js
+++ automation/gulpfile.js
@@ -3,17 +3,19 @@ const fs = require("fs");
 const gulp = require("gulp");
 const jsdoc2md = require("jsdoc-to-markdown");
 
+const files = "../source/*.jsx";
+
 gulp.task("build", done => {
     jsdoc2md.render({
-        template: fs.readFileSync("./template.hbs", "utf8"),
-        helper: "./replace.js",
-        files: `../source/*.jsx`})
+        "template": fs.readFileSync("./template.hbs", "utf8"),
+        "helper": "./replace.js",
+        "files": files})
     .then(output => fs.writeFileSync(`../README.md`, output));
     return done();
 });
 
 gulp.task("lint", () => {
-    return gulp.src([`../source/*.jsx`])
+    return gulp.src([files])
         .pipe(eslint({configFile: ".eslintrc.json"}))
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
